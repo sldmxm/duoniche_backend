cd7cb99724bc80d29abd48e5296d4f6cfe0649e9
Maxim Solodilov <sldmxm@gmail.com>
2025-05-05 21:19:51 +0300

    feat: model user_bot_profiles

---
+++ app/db/models/user_bot_profile.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import (
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+)
+from sqlalchemy import (
+    Enum as SQLAlchemyEnum,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func  # Для server_default и onupdate
+
+from app.core.entities.user_bot_profile import BotID, UserStatusInBot
+from app.db.base import Base
+
+if TYPE_CHECKING:
+    from app.db.models.user import User
+
+
+class UserBotProfile(Base):
+    __tablename__ = 'user_bot_profiles'
+
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('users.user_id', ondelete='CASCADE'),
+        primary_key=True,
+        index=True,
+    )
+    bot_id: Mapped[BotID] = mapped_column(
+        SQLAlchemyEnum(BotID, name='bot_id_enum', create_type=True),
+        primary_key=True,
+        index=True,
+        default=BotID.BG,
+    )
+
+    # user_language: Mapped[str] = mapped_column(String, nullable=False)
+    # language_level: Mapped[LanguageLevel] = mapped_column(
+    #     SQLAlchemyEnum(
+    #         LanguageLevel, name='language_level_enum', create_type=True
+    #     ),
+    #     nullable=False,
+    #     default=DEFAULT_LANGUAGE_LEVEL,
+    # )
+    status: Mapped[UserStatusInBot] = mapped_column(
+        SQLAlchemyEnum(
+            UserStatusInBot, name='user_status_in_bot_enum', create_type=True
+        ),
+        nullable=False,
+        default=UserStatusInBot.ACTIVE,
+        index=True,
+    )
+    reason: Mapped[str | None] = mapped_column(String, nullable=True)
+    # exercises_get_in_session: Mapped[int] = mapped_column(Integer, default=0)
+    # exercises_get_in_set: Mapped[int] = mapped_column(Integer, default=0)
+    # errors_count_in_set: Mapped[int] = mapped_column(Integer, default=0)
+    # last_exercise_at: Mapped[datetime] = mapped_column(
+    #     DateTime(timezone=True), nullable=True
+    # )
+    # session_started_at: Mapped[datetime] = mapped_column(
+    #     DateTime(timezone=True), nullable=True
+    # )
+    # session_frozen_until: Mapped[datetime] = mapped_column(
+    #     DateTime(timezone=True), nullable=True
+    # )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+    user: Mapped['User'] = relationship(back_populates='bot_profiles')
+
+    def __repr__(self) -> str:
+        return (
+            f'<UserBotProfile(user_id={self.user_id}, '
+            f"bot_id='{self.bot_id.value}', "
+            f"status='{self.status.value}')>"
+        )
